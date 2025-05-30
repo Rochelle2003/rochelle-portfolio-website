4d3a4177838faa4cf26ba02719e50c6c83299789
Rochelle2003 <rochellemannie2003@outlook.com>
2025-05-30 12:38:50 +0200

    favicon add

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,16 @@
-
 import '../styles/globals.css';
 import Header from '../components/Header';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
   );
 }
+
